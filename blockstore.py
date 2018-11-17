1abacd414093283dee753355f3a1ec026a7799c5
Jimmy Young <yangjufo@gmail.com>
2018-11-17 19:21:12 +0000

    add log function in blockstore

--- blockstore.py
+++ blockstore.py
@@ -6,7 +6,7 @@ import logging
 The BlockStore service is an in-memory data store that stores blocks of data,
 indexed by the hash value.  Thus it is a key-value store. It supports basic
 get() and put() operations. It does not need to support deleting blocks of
-data–we just let unused blocks remain in the store. The BlockStore service only knows about blocks–it doesn’t know anything about how blocks relate to files.
+data we just let unused blocks remain in the store. The BlockStore service only knows about blocks it doesn't know anything about how blocks relate to files.
 """
 class BlockStore(rpyc.Service):
 
@@ -16,13 +16,7 @@ class BlockStore(rpyc.Service):
     """
     def __init__(self):
         self.hash_blocks = {}        
-        self.logger = logging.getLogger('blockstore')
-        hdlr = logging.FileHandler('blockstore.log')
-        formatter = logging.Formatter('%(asctime)s %levelname)s %(message)s')
-        hdlr.setFormatter(formatter)
-        self.logger.addHandler(hdlr)
-        self.logger.setLevel(logging.WARNING)
-
+        logging.basicConfig(filename='blockstore.log', level=logging.DEBUG, format='%(asctime)s  %(message)s', datefmt='%m%d%Y %I:%M:%S %p')
 
     """
     store_block(h, b) : Stores block b in the key-value store, indexed by
@@ -32,7 +26,7 @@ class BlockStore(rpyc.Service):
     method as an RPC call
     """
     def exposed_store_block(self, h, block):        
-        self.logger.info("store block: " + str(h))
+        logging.info("store block: " + str(h))
         self.hash_blocks[h] = block
         return True
         
@@ -45,7 +39,7 @@ class BlockStore(rpyc.Service):
     method as an RPC call
     """
     def exposed_get_block(self, h):
-        self.logger.info("get block: " + str(h))
+        logging.info("get block: " + str(h))
         return self.hash_blocks[h]
 
     """
@@ -56,7 +50,7 @@ class BlockStore(rpyc.Service):
     method as an RPC call
     """
     def exposed_has_block(self, h):
-        self.logger.info("has block: " + str(h))
+        logging.info("has block: " + str(h))
         return h in self.hash_blocks
 		
 if __name__ == '__main__':
